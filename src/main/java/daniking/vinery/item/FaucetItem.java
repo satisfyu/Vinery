be83ff1e60fab95361098454b5085ad2c4d6a26d
satisfyu <104036234+satisfyu@users.noreply.github.com>
2022-08-29 19:57:16 +0200

    Update FaucetItem.java

--- src/main/java/daniking/vinery/item/FaucetItem.java
+++ src/main/java/daniking/vinery/item/FaucetItem.java
@@ -1,11 +1,12 @@
 package daniking.vinery.item;
 
-import daniking.vinery.registry.ObjectRegistry;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Rarity;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -14,12 +15,20 @@ import java.util.List;
 
 public class FaucetItem extends Item {
 	
-	public FaucetItem(Settings settings) {
+	public FaucetItem(Rarity uncommon, Settings settings) {
 		super(settings);
 	}
 	
 	@Override
 	public void appendTooltip(ItemStack stack, @Nullable World world, @NotNull List<Text> tooltip, TooltipContext context) {
-		tooltip.add(Text.of("Used on " + ObjectRegistry.KITCHEN_SINK.getName().getString()));
+		tooltip.add(Text.of("Can be placed on a kitchen sink."));
+
+		if (Screen.hasShiftDown()) {
+			tooltip.add(new TranslatableText("item.vinery.faucet.tooltip.shift"));
+		} else {
+			tooltip.add(new TranslatableText("item.vinery.faucet.tooltip"));
 	}
-}
+
+
+}}
+
