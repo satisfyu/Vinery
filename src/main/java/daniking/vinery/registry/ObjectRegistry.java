b2b6431d8395497b6a4451c4fb6ab6a915b28bc1
satisfyu <104036234+satisfyu@users.noreply.github.com>
2022-11-26 00:16:27 +0100

    bread slice added

--- src/main/java/daniking/vinery/registry/ObjectRegistry.java
+++ src/main/java/daniking/vinery/registry/ObjectRegistry.java
@@ -199,6 +199,7 @@ public class ObjectRegistry {
     public static final Item DONUT = register("donut", new DoughnutItem(getSettings().food(FoodComponents.CARROT)));
     public static final Item MILK_BREAD = register("milk_bread", new MilkBreadItem(getSettings().food(FoodComponents.COOKIE)));
     public static final Block CRUSTY_BREAD = register("crusty_bread", new BreadBlock(AbstractBlock.Settings.copy(Blocks.CAKE).nonOpaque()));
+    public static final Item BREAD_SLICE = register("bread_slice", new Item(getSettings().food(FoodComponents.BAKED_POTATO)));
     public static final Block APPLE_PIE = register("apple_pie", new PieBlock((FabricBlockSettings.copyOf(Blocks.CAKE)), ObjectRegistry.APPLE_PIE_SLICE, false));
     public static final Item APPLE_PIE_SLICE = register("apple_pie_slice", new Item(getSettings().food(FoodComponents.COOKED_BEEF)));
     public static final Item APPLE_MASH = register("apple_mash", new CherryItem(getSettings().food(FoodComponents.APPLE)));
