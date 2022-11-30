86a76479a9b28b61928a9c476d717e9b18749fad
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2022-11-30 15:11:38 +0100

    built in bushy leaves resource pack

--- src/main/java/daniking/vinery/Vinery.java
+++ src/main/java/daniking/vinery/Vinery.java
@@ -5,12 +5,17 @@ import daniking.vinery.world.VineryConfiguredFeatures;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.fabric.impl.resource.loader.ModNioResourcePack;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.entry.LootTableEntry;
+import net.minecraft.resource.ResourceType;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -44,6 +49,10 @@ public class Vinery implements ModInitializer {
         VinerySoundEvents.init();
         VineryVillagers.init();
         VineryEntites.init();
+
+        FabricLoader.getInstance().getModContainer(MODID).ifPresent(container -> {
+            ResourceManagerHelper.registerBuiltinResourcePack(new VineryIdentifier("bushy_leaves"), container, ResourcePackActivationType.DEFAULT_ENABLED);
+        });
     }
 }
 
