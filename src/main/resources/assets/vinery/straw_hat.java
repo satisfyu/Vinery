4c9b9e7932dfd715f696ceb8993b042a5e447db6
satisfyu <104036234+satisfyu@users.noreply.github.com>
2023-01-04 12:36:58 +0100

    hat

--- src/main/resources/assets/vinery/straw_hat.java
+++ src/main/resources/assets/vinery/straw_hat.java
@@ -3,31 +3,21 @@
 // Paste this class into your mod and generate all required imports
 
 
-public class unknown<T extends Entity> extends EntityModel<T> {
+public class custom_model<T extends Entity> extends EntityModel<T> {
 	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
-	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "unknown"), "main");
-	private final ModelPart bipedHead;
-	private final ModelPart bipedLeftLeg;
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "custom_model"), "main");
+	private final ModelPart bb_main;
 
-	public unknown(ModelPart root) {
-		this.bipedHead = root.getChild("bipedHead");
-		this.bipedLeftLeg = root.getChild("bipedLeftLeg");
+	public custom_model(ModelPart root) {
+		this.bb_main = root.getChild("bb_main");
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition bipedHead = partdefinition.addOrReplaceChild("bipedHead", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, 0.0F));
-
-		PartDefinition armorHead = bipedHead.addOrReplaceChild("armorHead", CubeListBuilder.create().texOffs(2, 1).addBox(-4.5F, 19.0F, -4.5F, 9.0F, 4.0F, 9.0F, new CubeDeformation(0.0F))
-		.texOffs(-16, 14).addBox(-8.0F, 23.0F, -8.0F, 16.0F, 0.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 1.0F, 0.0F));
-
-		PartDefinition bipedLeftLeg = partdefinition.addOrReplaceChild("bipedLeftLeg", CubeListBuilder.create(), PartPose.offset(-2.0F, 12.0F, 0.0F));
-
-		PartDefinition armorLeftLeg = bipedLeftLeg.addOrReplaceChild("armorLeftLeg", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, 0.0F));
-
-		PartDefinition armorLeftBoot = bipedLeftLeg.addOrReplaceChild("armorLeftBoot", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, 0.0F));
+		PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(-16, 14).addBox(-8.0F, -7.0F, -8.0F, 16.0F, 0.0F, 16.0F, new CubeDeformation(0.0F))
+		.texOffs(2, 1).addBox(-4.5F, -11.001F, -4.5F, 9.0F, 4.0F, 9.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
 
 		return LayerDefinition.create(meshdefinition, 64, 64);
 	}
@@ -39,7 +29,6 @@ public class unknown<T extends Entity> extends EntityModel<T> {
 
 	@Override
 	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-		bipedHead.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-		bipedLeftLeg.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		bb_main.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 	}
 }
