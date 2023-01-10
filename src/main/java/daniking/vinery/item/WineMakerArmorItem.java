4689684b3e7b0c458163d82d2f8ac1138bbe7f79
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2023-01-10 18:37:39 +0100

    composter

--- src/main/java/daniking/vinery/item/WineMakerArmorItem.java
+++ src/main/java/daniking/vinery/item/WineMakerArmorItem.java
@@ -20,18 +20,23 @@ public interface WineMakerArmorItem {
         ItemStack chestplate = player.getEquippedStack(EquipmentSlot.CHEST);
         ItemStack leggings = player.getEquippedStack(EquipmentSlot.LEGS);
         ItemStack boots = player.getEquippedStack(EquipmentSlot.FEET);
+        boolean helmetB = helmet != null && helmet.getItem() instanceof WineMakerArmorItem;
+        boolean chestplateB = chestplate != null && chestplate.getItem() instanceof WineMakerArmorItem;
+        boolean leggingsB = leggings != null && leggings.getItem() instanceof WineMakerArmorItem;
+        boolean bootsB = boots != null && boots.getItem() instanceof WineMakerArmorItem;
+
         tooltip.add(Text.of(""));
         tooltip.add(Text.of(Formatting.AQUA + I18n.translate("vinery.tooltip.winemaker_armor")));
-        tooltip.add(Text.of((helmet != null && helmet.getItem() instanceof WineMakerArmorItem ? Formatting.GREEN.toString() : Formatting.GRAY.toString()) + "- [" + ObjectRegistry.STRAW_HAT.getName().getString() + "]"));
-        tooltip.add(Text.of((chestplate != null && chestplate.getItem() instanceof WineMakerArmorItem ? Formatting.GREEN.toString() : Formatting.GRAY.toString()) + "- [" + ObjectRegistry.VINEMAKER_APRON.getName().getString() + "]"));
-        tooltip.add(Text.of((leggings != null && leggings.getItem() instanceof WineMakerArmorItem ? Formatting.GREEN.toString() : Formatting.GRAY.toString()) + "- [" + ObjectRegistry.VINEMAKER_LEGGINGS.getName().getString() + "]"));
-        tooltip.add(Text.of((boots != null && boots.getItem() instanceof WineMakerArmorItem ? Formatting.GREEN.toString() : Formatting.GRAY.toString()) + "- [" + ObjectRegistry.VINEMAKER_BOOTS.getName().getString() + "]"));
+        tooltip.add(Text.of((helmetB ? Formatting.GREEN.toString() : Formatting.GRAY.toString()) + "- [" + ObjectRegistry.STRAW_HAT.getName().getString() + "]"));
+        tooltip.add(Text.of((chestplateB ? Formatting.GREEN.toString() : Formatting.GRAY.toString()) + "- [" + ObjectRegistry.VINEMAKER_APRON.getName().getString() + "]"));
+        tooltip.add(Text.of((leggingsB ? Formatting.GREEN.toString() : Formatting.GRAY.toString()) + "- [" + ObjectRegistry.VINEMAKER_LEGGINGS.getName().getString() + "]"));
+        tooltip.add(Text.of((bootsB ? Formatting.GREEN.toString() : Formatting.GRAY.toString()) + "- [" + ObjectRegistry.VINEMAKER_BOOTS.getName().getString() + "]"));
         tooltip.add(Text.of(""));
         tooltip.add(Text.of(Formatting.GRAY + I18n.translate("vinery.tooltip.winemaker_armor2")));
-        tooltip.add(Text.of(((helmet != null && helmet.getItem() instanceof WineMakerArmorItem &&
-                chestplate != null && chestplate.getItem() instanceof WineMakerArmorItem &&
-                leggings != null && leggings.getItem() instanceof WineMakerArmorItem &&
-                boots != null && boots.getItem() instanceof WineMakerArmorItem) ? Formatting.DARK_GREEN.toString() : Formatting.GRAY.toString()) + I18n.translate("vinery.tooltip.winemaker_armor3")));
+        tooltip.add(Text.of(((helmetB &&
+                chestplateB &&
+                leggingsB &&
+                bootsB) ? Formatting.DARK_GREEN.toString() : Formatting.GRAY.toString()) + I18n.translate("vinery.tooltip.winemaker_armor3")));
     }
 
 }
