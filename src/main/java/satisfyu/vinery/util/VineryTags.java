320d475525eed1326c42f5c9854c647d8c4b8477
satisfyu <104036234+satisfyu@users.noreply.github.com>
2023-03-11 13:54:19 +0100

    wine tags

--- src/main/java/satisfyu/vinery/util/VineryTags.java
+++ src/main/java/satisfyu/vinery/util/VineryTags.java
@@ -11,11 +11,10 @@ public class VineryTags {
 
     public static final TagKey<Block> ALLOWS_COOKING_ON_POT = TagKey.of(Registry.BLOCK_KEY, new VineryIdentifier("allows_cooking_on_pot"));
     public static final TagKey<Block> CAN_NOT_CONNECT = TagKey.of(Registry.BLOCK_KEY, new VineryIdentifier("can_not_connect"));
-
+    public static final TagKey<Block> WINE_RACK = TagKey.of(Registry.BLOCK_KEY, new VineryIdentifier("wine_racks"));
     public static final TagKey<Item> IGNORE_BLOCK_ITEM = TagKey.of(Registry.ITEM_KEY, new VineryIdentifier("ignore_block_item"));
     public static final TagKey<Item> CHERRY_LOGS = TagKey.of(Registry.ITEM_KEY, new VineryIdentifier("cherry_logs"));
     public static final TagKey<Item> JAMS = TagKey.of(Registry.ITEM_KEY, new VineryIdentifier("jams"));
     public static final TagKey<Item> SMALL_FLOWER = TagKey.of(Registry.ITEM_KEY, new VineryIdentifier( "small_flower"));
-    public static final TagKey<Block> WINE_RACK = TagKey.of(Registry.BLOCK_KEY, new VineryIdentifier("wine_racks"));
-
+    public static final TagKey<Item> WINE = TagKey.of(Registry.ITEM_KEY, new VineryIdentifier("wine"));
 }
