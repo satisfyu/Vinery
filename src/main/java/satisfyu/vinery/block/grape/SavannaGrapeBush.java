cd7257f376cbb5c20128405505695529b588cb1e
satisfyu <104036234+satisfyu@users.noreply.github.com>
2023-03-18 19:57:46 +0100

    Update SavannaGrapeBush.java

--- src/main/java/satisfyu/vinery/block/grape/SavannaGrapeBush.java
+++ src/main/java/satisfyu/vinery/block/grape/SavannaGrapeBush.java
@@ -1,7 +1,10 @@
 package satisfyu.vinery.block.grape;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.WorldView;
 import satisfyu.vinery.util.GrapevineType;
 
@@ -15,4 +18,11 @@ public class SavannaGrapeBush extends GrapeBush {
     public boolean canGrowPlace(WorldView world, BlockPos blockPos, BlockState blockState) {
         return world.getBaseLightLevel(blockPos, 0) >= 14;
     }
+
+    public static VoxelShape makeShape() {
+        VoxelShape shape = VoxelShapes.empty();
+        shape = VoxelShapes.combine(shape, VoxelShapes.cuboid(0, 0, 0, 1, 1, 1), BooleanBiFunction.OR);
+        return shape;
+    }
+
 }
