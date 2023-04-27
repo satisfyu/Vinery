369e2e6a8dd0db6a86573b0c37845f27ffe4a22f
satisfyu <104036234+satisfyu@users.noreply.github.com>
2023-04-27 22:10:03 +0200

    winebottlereturn

--- common/src/main/java/satisfyu/vinery/item/JuiceItem.java
+++ common/src/main/java/satisfyu/vinery/item/JuiceItem.java
@@ -1,24 +1,71 @@
 package satisfyu.vinery.item;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
-import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.HoneyBottleItem;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import satisfyu.vinery.registry.ObjectRegistry;
 
-public class JuiceItem extends ConsumAndReturnItem {
-    public JuiceItem(Properties settings) {
-        super(settings, 40, ObjectRegistry.WINE_BOTTLE, true);
+
+public class JuiceItem extends Item {
+    private static final int DRINK_DURATION = 40;
+
+    public JuiceItem(Item.Properties arg) {
+        super(arg);
     }
 
-    @Override
-    public void appendHoverText(ItemStack stack, @Nullable Level world, @NotNull List<Component> tooltip, TooltipFlag context) {
-        tooltip.add(Component.translatable("item.vinery.juice.tooltip." + this.getDescriptionId()).withStyle(ChatFormatting.ITALIC, ChatFormatting.GRAY));
+    public ItemStack finishUsingItem(ItemStack arg, Level arg2, LivingEntity arg3) {
+        super.finishUsingItem(arg, arg2, arg3);
+        if (arg3 instanceof ServerPlayer serverPlayer) {
+            CriteriaTriggers.CONSUME_ITEM.trigger(serverPlayer, arg);
+            serverPlayer.awardStat(Stats.ITEM_USED.get(this));
+        }
+
+        if (!arg2.isClientSide) {
+            arg3.removeEffect(MobEffects.POISON);
+        }
+
+        if (arg.isEmpty()) {
+            return new ItemStack(ObjectRegistry.WINE_BOTTLE.get(), 1);
+        } else {
+            if (arg3 instanceof Player && !((Player)arg3).getAbilities().instabuild) {
+                ItemStack itemStack = new ItemStack(ObjectRegistry.WINE_BOTTLE.get(), 1);
+                Player player = (Player)arg3;
+                if (!player.getInventory().add(itemStack)) {
+                    player.drop(itemStack, false);
+                }
+            }
+            arg.shrink(1);
+            return arg;
+        }
+    }
+
+    public int getUseDuration(ItemStack arg) {
+        return 40;
+    }
+
+    public UseAnim getUseAnimation(ItemStack arg) {
+        return UseAnim.DRINK;
+    }
+
+    public SoundEvent getDrinkingSound() {
+        return SoundEvents.HONEY_DRINK;
+    }
+
+    public SoundEvent getEatingSound() {
+        return SoundEvents.HONEY_DRINK;
+    }
+
+    public InteractionResultHolder<ItemStack> use(Level arg, Player arg2, InteractionHand arg3) {
+        return ItemUtils.startUsingInstantly(arg, arg2, arg3);
     }
 }
