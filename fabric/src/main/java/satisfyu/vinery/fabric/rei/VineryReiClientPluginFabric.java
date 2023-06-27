a86836c584d869b5820524fd140334c2ba8dc776
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2023-06-27 15:20:27 +0200

    REi update

---
+++ fabric/src/main/java/satisfyu/vinery/fabric/rei/VineryReiClientPluginFabric.java
@@ -0,0 +1,19 @@
+package satisfyu.vinery.fabric.rei;
+
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
+import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import satisfyu.vinery.compat.rei.VineryReiClientPlugin;
+
+
+public class VineryReiClientPluginFabric implements REIClientPlugin {
+    @Override
+    public void registerCategories(CategoryRegistry registry) {
+        VineryReiClientPlugin.registerCategories(registry);
+    }
+
+    @Override
+    public void registerDisplays(DisplayRegistry registry) {
+        VineryReiClientPlugin.registerDisplays(registry);
+    }
+}
