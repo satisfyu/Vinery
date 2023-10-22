f89b701238a31cd82c9b65ef7e03a8a796010f1b
IamJannik <jannik.ohme@gmail.com>
2023-10-22 15:33:34 +0200

    MoreGrapeTypes+

--- common/src/main/java/satisfyu/vinery/block/grape/GrapeProperty.java
+++ common/src/main/java/satisfyu/vinery/block/grape/GrapeProperty.java
@@ -1,30 +1,19 @@
 package satisfyu.vinery.block.grape;
 
-import com.google.common.collect.Maps;
 import net.minecraft.world.level.block.state.properties.Property;
 import org.jetbrains.annotations.NotNull;
 import satisfyu.vinery.registry.GrapeTypes;
 
 import java.util.Collection;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
 public class GrapeProperty extends Property<GrapeType> {
     private final Set<GrapeType> values;
-    private final Map<String, GrapeType> names = Maps.newHashMap();
 
     protected GrapeProperty(String name) {
         super(name, GrapeType.class);
         this.values = GrapeTypes.GRAPE_TYPE_TYPES;
-
-        for (GrapeType grapeType : this.values) {
-            String id = grapeType.getSerializedName();
-            if (this.names.containsKey(id)) {
-                throw new IllegalArgumentException("Multiple values have the same name '" + id + "'");
-            }
-            this.names.put(id, grapeType);
-        }
     }
 
     public static GrapeProperty create(String name) {
@@ -43,6 +32,10 @@ public class GrapeProperty extends Property<GrapeType> {
 
     @Override
     public @NotNull Optional<GrapeType> getValue(String string) {
-        return Optional.ofNullable(this.names.get(string));
+        for (GrapeType grapeType : values) {
+            if (string.equals(grapeType.getSerializedName()))
+                return Optional.of(grapeType);
+        }
+        return Optional.empty();
     }
 }
