bd552834ac655d97d1783131f96920cefc4a83aa
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2023-11-12 22:54:57 +0100

    Update VineryCompostables.java

--- common/src/main/java/satisfyu/vinery/registry/VineryCompostables.java
+++ common/src/main/java/satisfyu/vinery/registry/VineryCompostables.java
@@ -1,33 +1,42 @@
 package satisfyu.vinery.registry;
 
+import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.ComposterBlock;
 
 public class VineryCompostables {
     public static void registerCompostable() {
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.WHITE_GRAPE.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.WHITE_GRAPE_SEEDS.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.RED_GRAPE.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.RED_GRAPE_SEEDS.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.CHERRY_LEAVES.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.GRAPEVINE_LEAVES.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.CHERRY.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.ROTTEN_CHERRY.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.APPLE_TREE_SAPLING.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.APPLE_LEAVES.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.CHERRY_SAPLING.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.APPLE_MASH.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.STRAW_HAT.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.JUNGLE_RED_GRAPE_SEEDS.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.JUNGLE_RED_GRAPE.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.JUNGLE_WHITE_GRAPE_SEEDS.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.JUNGLE_WHITE_GRAPE.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.TAIGA_RED_GRAPE_SEEDS.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.TAIGA_RED_GRAPE.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.TAIGA_WHITE_GRAPE_SEEDS.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.TAIGA_WHITE_GRAPE.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.SAVANNA_RED_GRAPE_SEEDS.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.SAVANNA_RED_GRAPE.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.SAVANNA_WHITE_GRAPE_SEEDS.get(), 0.4F);
-        ComposterBlock.COMPOSTABLES.put(ObjectRegistry.SAVANNA_WHITE_GRAPE.get(), 0.4F);
+        registerCompostableItem(ObjectRegistry.WHITE_GRAPE, 0.4F);
+        registerCompostableItem(ObjectRegistry.WHITE_GRAPE_SEEDS, 0.4F);
+        registerCompostableItem(ObjectRegistry.RED_GRAPE, 0.4F);
+        registerCompostableItem(ObjectRegistry.RED_GRAPE_SEEDS, 0.4F);
+        registerCompostableItem(ObjectRegistry.CHERRY_LEAVES, 0.4F);
+        registerCompostableItem(ObjectRegistry.GRAPEVINE_LEAVES, 0.4F);
+        registerCompostableItem(ObjectRegistry.CHERRY, 0.4F);
+        registerCompostableItem(ObjectRegistry.ROTTEN_CHERRY, 0.4F);
+        registerCompostableItem(ObjectRegistry.APPLE_TREE_SAPLING, 0.4F);
+        registerCompostableItem(ObjectRegistry.APPLE_LEAVES, 0.4F);
+        registerCompostableItem(ObjectRegistry.CHERRY_SAPLING, 0.4F);
+        registerCompostableItem(ObjectRegistry.APPLE_MASH, 0.4F);
+        registerCompostableItem(ObjectRegistry.STRAW_HAT, 0.4F);
+        registerCompostableItem(ObjectRegistry.JUNGLE_RED_GRAPE_SEEDS, 0.4F);
+        registerCompostableItem(ObjectRegistry.JUNGLE_RED_GRAPE, 0.4F);
+        registerCompostableItem(ObjectRegistry.JUNGLE_WHITE_GRAPE_SEEDS, 0.4F);
+        registerCompostableItem(ObjectRegistry.JUNGLE_WHITE_GRAPE, 0.4F);
+        registerCompostableItem(ObjectRegistry.TAIGA_RED_GRAPE_SEEDS, 0.4F);
+        registerCompostableItem(ObjectRegistry.TAIGA_RED_GRAPE, 0.4F);
+        registerCompostableItem(ObjectRegistry.TAIGA_WHITE_GRAPE_SEEDS, 0.4F);
+        registerCompostableItem(ObjectRegistry.TAIGA_WHITE_GRAPE, 0.4F);
+        registerCompostableItem(ObjectRegistry.SAVANNA_RED_GRAPE_SEEDS, 0.4F);
+        registerCompostableItem(ObjectRegistry.SAVANNA_RED_GRAPE, 0.4F);
+        registerCompostableItem(ObjectRegistry.SAVANNA_WHITE_GRAPE_SEEDS, 0.4F);
+        registerCompostableItem(ObjectRegistry.SAVANNA_WHITE_GRAPE, 0.4F);
+    }
+
+    public static <T extends ItemLike> void registerCompostableItem(RegistrySupplier<T> item, float chance) {
+        if (item.get().asItem() != Items.AIR) {
+            ComposterBlock.COMPOSTABLES.put(item.get(), chance);
+        }
     }
 }
