94562f56e3718b5715196f49c68df00777029be1
satisfyu <104036234+satisfyu@users.noreply.github.com>
2024-03-24 11:50:01 +0100

    Fixed: Savanna Grapes causing a NPE

--- common/src/main/java/satisfyu/vinery/block/grape/SavannaGrapeBush.java
+++ common/src/main/java/satisfyu/vinery/block/grape/SavannaGrapeBush.java
@@ -1,22 +1,11 @@
 package satisfyu.vinery.block.grape;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.shapes.BooleanOp;
-import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.Shapes;
-import net.minecraft.world.phys.shapes.VoxelShape;
-import org.jetbrains.annotations.NotNull;
-import satisfyu.vinery.util.Util;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Supplier;
 
 public class SavannaGrapeBush extends GrapeBush {
+
     public SavannaGrapeBush(Properties settings, GrapeType type) {
         super(settings, type, 3);
     }
@@ -24,26 +13,5 @@ public class SavannaGrapeBush extends GrapeBush {
     @Override
     public boolean canGrowPlace(LevelReader world, BlockPos blockPos, BlockState blockState) {
         return world.getRawBrightness(blockPos, 0) >= 14;
-    }
-
-
-    private static final Supplier<VoxelShape> voxelShapeSupplier = () -> {
-        VoxelShape shape = Shapes.empty();
-        shape = Shapes.joinUnoptimized(shape, Shapes.box(0.125, 0.3125, 0.4375, 0.125, 0.8125, 0.5625), BooleanOp.OR);
-        shape = Shapes.joinUnoptimized(shape, Shapes.box(0.875, 0.3125, 0.4375, 0.875, 0.8125, 0.5625), BooleanOp.OR);
-        shape = Shapes.joinUnoptimized(shape, Shapes.box(0.125, 0.8125, 0.4375, 0.875, 0.8125, 0.5625), BooleanOp.OR);
-        shape = Shapes.joinUnoptimized(shape, Shapes.box(0.125, 0, 0.1875, 0.875, 0.4375, 0.8125), BooleanOp.OR);
-        return shape;
-    };
-
-    public static final Map<Direction, VoxelShape> SHAPE = net.minecraft.Util.make(new HashMap<>(), map -> {
-        for (Direction direction : Direction.Plane.HORIZONTAL.stream().toList()) {
-            map.put(direction, Util.rotateShape(Direction.NORTH, direction, voxelShapeSupplier.get()));
-        }
-    });
-
-    @Override
-    public @NotNull VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-        return SHAPE.get(null);
     }
 }
