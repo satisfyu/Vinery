0d6cff65ab655008bad635ac0a9462bdf0119932
satisfy <104036234+satisfyu@users.noreply.github.com>
2026-05-16 23:35:33 +0200

    chore: update pack.png

--- common/src/main/java/net/satisfy/vinery/core/block/WindowBlock.java
+++ common/src/main/java/net/satisfy/vinery/core/block/WindowBlock.java
@@ -21,90 +21,98 @@ public class WindowBlock extends IronBarsBlock {
 
     public WindowBlock(BlockBehaviour.Properties settings) {
         super(settings);
-        this.registerDefaultState(this.stateDefinition.any().setValue(NORTH, false).setValue(EAST, false).setValue(SOUTH, false).setValue(WEST, false).setValue(WATERLOGGED, false));
+        this.registerDefaultState(this.stateDefinition.any()
+                .setValue(NORTH, false)
+                .setValue(EAST, false)
+                .setValue(SOUTH, false)
+                .setValue(WEST, false)
+                .setValue(WATERLOGGED, false)
+                .setValue(PART, 0));
     }
 
+    @Override
     public void setPlacedBy(Level world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
         if (!world.isClientSide()) {
-            this.updateWindows(world, pos, state);
-        }
 
+            this.updateWindows(world, pos);
+        }
     }
 
+    @Override
     public @NotNull BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor world, BlockPos pos, BlockPos neighborPos) {
         if (state.getValue(WATERLOGGED)) {
             world.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(world));
         }
-
         return super.updateShape(state, direction, neighborState, world, pos, neighborPos);
     }
 
-    private void updateWindows(LevelAccessor world, BlockPos pos, BlockState state) {
+    private void updateWindows(LevelAccessor world, BlockPos pos) {
         BlockPos lowest = this.getLowestWindow(world, pos);
         BlockPos highest = this.getHighestWindow(world, pos);
         int height = 0;
+        BlockPos current = lowest;
 
-        BlockPos current;
-        for(current = lowest; current.compareTo(highest) <= 0; current = current.above()) {
-            ++height;
+        while (current.compareTo(highest) <= 0) {
+            height++;
+            current = current.above();
         }
 
         if (height == 1) {
-            world.setBlock(lowest, state.setValue(PART, 0), 3);
+            BlockState s0 = world.getBlockState(lowest);
+            world.setBlock(lowest, s0.setValue(PART, 0), 11);
         } else if (height == 2) {
-            world.setBlock(lowest, state.setValue(PART, 1), 3);
-            world.setBlock(highest, state.setValue(PART, 3), 3);
+            BlockState s0 = world.getBlockState(lowest);
+            BlockState s1 = world.getBlockState(highest);
+            world.setBlock(lowest, s0.setValue(PART, 1), 11);
+            world.setBlock(highest, s1.setValue(PART, 3), 11);
         } else {
-            world.setBlock(lowest, state.setValue(PART, 1), 3);
-
-            for(current = lowest.above(); current.compareTo(highest) < 0; current = current.above()) {
-                world.setBlock(current, state.setValue(PART, 2), 3);
+            BlockState s0 = world.getBlockState(lowest);
+            world.setBlock(lowest, s0.setValue(PART, 1), 11);
+            for (current = lowest.above(); current.compareTo(highest) < 0; current = current.above()) {
+                BlockState sm = world.getBlockState(current);
+                world.setBlock(current, sm.setValue(PART, 2), 11);
             }
-
-            world.setBlock(highest, state.setValue(PART, 3), 3);
+            BlockState sN = world.getBlockState(highest);
+            world.setBlock(highest, sN.setValue(PART, 3), 11);
         }
-
     }
 
     private BlockPos getLowestWindow(LevelAccessor world, BlockPos pos) {
-        while(world.getBlockState(pos.below()).getBlock() == this) {
+        while (world.getBlockState(pos.below()).getBlock() == this) {
             pos = pos.below();
         }
-
         return pos;
     }
 
     private BlockPos getHighestWindow(LevelAccessor world, BlockPos pos) {
-        while(world.getBlockState(pos.above()).getBlock() == this) {
+        while (world.getBlockState(pos.above()).getBlock() == this) {
             pos = pos.above();
         }
-
         return pos;
     }
 
+    @Override
     public void neighborChanged(BlockState state, Level world, BlockPos pos, Block block, BlockPos fromPos, boolean isMoving) {
         super.neighborChanged(state, world, pos, block, fromPos, isMoving);
         if (!world.isClientSide()) {
             this.updatePartOnNeighborChange(world, pos, state);
         }
-
     }
 
     private void updatePartOnNeighborChange(Level world, BlockPos pos, BlockState state) {
         boolean hasBelow = world.getBlockState(pos.below()).getBlock() == this;
         boolean hasAbove = world.getBlockState(pos.above()).getBlock() == this;
         if (!hasBelow && !hasAbove) {
-            world.setBlock(pos, state.setValue(PART, 0), 3);
+            world.setBlock(pos, state.setValue(PART, 0), 11);
         } else if (!hasBelow) {
-            world.setBlock(pos, state.setValue(PART, 1), 3);
+            world.setBlock(pos, state.setValue(PART, 1), 11);
         } else if (!hasAbove) {
-            world.setBlock(pos, state.setValue(PART, 3), 3);
+            world.setBlock(pos, state.setValue(PART, 3), 11);
         }
-
     }
 
+    @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(PART, NORTH, EAST, WEST, SOUTH, WATERLOGGED);
     }
 }
-
